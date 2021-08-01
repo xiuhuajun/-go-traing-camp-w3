f9f8f62b5566f448d83fbeea2d388c30ea878f56
xiuhuajun <1716211499@qq.com>
2021-08-02 00:52:27 +0800

    Update server.go

--- server.go
+++ server.go
@@ -1,17 +1,27 @@
-//启动 HTTP server
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"golang.org/x/sync/errgroup"
+)
+
 func StartHttpServer(srv *http.Server) error {
-	http.HandleFunc("/hello", HelloServer2)
+	http.HandleFunc("/hello", HelloServer)
 	fmt.Println("http server start")
 	err := srv.ListenAndServe()
 	return err
 }
 
-// 增加一个 HTTP hanlder
-func HelloServer2(w http.ResponseWriter, req *http.Request) {
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello, world!\n")
 }
 
-//1. 基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
 func main() {
 	ctx := context.Background()
 	// 定义 withCancel -> cancel() 方法 去取消下游的 Context
@@ -26,20 +36,20 @@ func main() {
 	})
 
 	group.Go(func() error {
-		<-errCtx.Done() //阻塞。因为 cancel、timeout、deadline 都可能导致 Done 被 close
+		<-errCtx.Done() 
 		fmt.Println("http server stop")
 		return srv.Shutdown(errCtx) // 关闭 http server
 	})
 
-	chanel := make(chan os.Signal, 1) //这里要用 buffer 为1的 chan
+	chanel := make(chan os.Signal, 1) 
 	signal.Notify(chanel)
 
 	group.Go(func() error {
 		for {
 			select {
-			case <-errCtx.Done(): // 因为 cancel、timeout、deadline 都可能导致 Done 被 close
+			case <-errCtx.Done(): 
 				return errCtx.Err()
-			case <-chanel: // 因为 kill -9 或其他而终止
+			case <-chanel: // kill -9 或其他而终止
 				cancel()
 			}
 		}
